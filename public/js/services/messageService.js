114f9431df61a27bfd72f206addb9d8042b006ad
Chris Rijos <rijos.christopher@gmail.com>
2016-10-11 15:36:34 -0500

    wip

--- public/js/services/messageService.js
+++ public/js/services/messageService.js
@@ -6,6 +6,15 @@
     Service.$inject = ["$scope", "$http"];
 
     function Service($scope, $http) {
-      
+        function getMessages() {
+          $http({
+            method: 'GET',
+            url: '/messages/show'
+          }).then(function onSuccess(response) {
+              console.log(response)
+          }, function onError(response) {
+              console.log(response)
+          })
+        }
     }
 })();

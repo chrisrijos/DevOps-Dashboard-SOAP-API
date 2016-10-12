1b0f2e2ab7837fd39e4cf9a6f69a91b93da329ad
Christopher Rijos <rijos.christopher@gmail.com>
2016-10-11 20:16:00 -0500

    wip

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -3,13 +3,12 @@
     .module('App')
     .controller('HomeController', MainController);
 
-    MainController.$inject = ["$scope", "$location", "MessageService"]
+    MainController.$inject = ["$scope", "$location", "MessageService"];
 
     function MainController($scope, $location, MessageService) {
         var HomeController = this;
 
         MessageService.getMessages().then(function (d) {
-            console.log(d)
             HomeController.data = d
         });
     };
